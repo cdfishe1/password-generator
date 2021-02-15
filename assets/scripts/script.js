a462096d7d7c56eb5b931a9dbfef2e0a96bf7484
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-02-14 22:00:03 -0600

    Return 15 random string to text box.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,31 +4,25 @@ const generateBtn = document.querySelector("#generate");
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const specials = ['', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ']'];
+const specials = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ']'];
 
-const randomNumber = () => {
-  return numbers[Math.floor(Math.random() * numbers.length)];
+let passwordLength = 15;
+
+let userChoices = [...lowerLetters, ...upperLetters, ...numbers, ...specials];
+
+const randomUserChoices = () => {
+  return userChoices[Math.floor(Math.random() * userChoices.length)]
 };
 
-const randomLowerLetter = () => {
-  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
-} 
-
-const randomUpperLetter = () => {
-  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
-
-}
-const randomSpecials = () => {
-  return specials[Math.floor(Math.random() * specials.length)];
-}
-
-let passwordLength = 10;
-
-
-
 const generatePassword = () => {
-  return randomSpecials;
-}
+  let newPassword = [];
+  for (i = 0; i < passwordLength; i++) {
+    newPassword.push(randomUserChoices());
+  }
+  return newPassword.join('');
+};
+
+console.log(generatePassword());
 
 
 // This function generates the password and writes it to the #password id.
