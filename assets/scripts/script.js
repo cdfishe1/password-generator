efe79ad7beee215717b4a6a601a14cb4c84a4bb4
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-02-16 14:24:13 -0600

    Reset length question if not proper.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,6 @@
+// The button that initiates the password generator
+const generateBtn = document.querySelector("#generate");
+
 //  The arrays of criteria the user can choose to generate a password
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -8,16 +11,20 @@ const specials = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '
 let userChoices = [];
 
 //*** Begin Password Generation ***/
-const intro = alert('Welcome to the Password Genterator!\nYou can choose from these criteria:\nlowercase letter(a,b,d)\nUpper Case Letters(A,B,C)\nNumbers(0-9)\nSpecial characters(like #$%)\nYou must choose at least one criteria\n\nPress ok to continue');
+
+const intro = alert('Welcome to the Password Genterator!\nYou must choose from at least one of these criteria:\nlowercase letter(a,b,d)\nUpper Case Letters(A,B,C)\nNumbers(0-9)\nSpecial characters(like #$%)\nYou must choose at least one criteria\n\nPress ok to continue');
 
 let promptLowerLetters = prompt('Would you like to include lower-case letters (such as a,b,c)?\nPress OK if Yes or press Cancel if no');
 let promptUpperLetters = prompt('Would you like to include upper-case letters (such as A,B,C)?\nPress OK if Yes or press Cancel if no');
 let promptNumbers = prompt('Would you like to include lower-case letters (such as 0,1,2)?\nPress OK if Yes or press Cancel if no');
 let promptSpecials = prompt('Would you like to include lower-case letters (such as #,$,%)?\nPress OK if Yes or press Cancel if no');
-let promptPasswordLength = prompt('How long would like your password to be?\nYour password must be between 8 and 128 characters.');
 
-// Converts the passwordLength from a string to a number
-let passwordLength = parseInt(promptPasswordLength, 10);
+
+
+// let promptPasswordLength = prompt('How long should your password be?\nYour password must be between 8 and 128 characters.');
+
+// Sets global passwordLength variable
+let passwordLength;
 
 const lowerCriteria = () => {
   if (promptLowerLetters === '') {
@@ -43,16 +50,27 @@ const specialsCriteria = () => {
   } 
 };
 
+const promptPasswordLength = () => {
+  let promptLength = prompt('How long should your password be?\nYour password must be between 8 and 128 characters.');
+  
+  if ((promptLength >= 8) && (promptLength <= 128)) {
+    passwordLength = parseInt(promptLength, 10);
+    alert('Good Length!');
+  } else {
+    promptPasswordLength();
+  }
+};
+
 lowerCriteria();
 upperCriteria();
 numbersCriteria();
 specialsCriteria();
+promptPasswordLength();
+
+
+
 
 const finalMessage = alert('Great! We have all your criteria. Press the Generate Password button below.');
-
-
-// The button that initiates the password generator
-const generateBtn = document.querySelector("#generate");
 
 
 // The function that will randomly select a member of the userChoices array
