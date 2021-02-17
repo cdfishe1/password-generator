eb107235397449896c9cca52a3a133993998fed7
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-02-17 11:04:50 -0600

    Enclose global items in criteria object. Refactor functions as needed.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,15 @@
 // The button that initiates the password generator
 const generateBtn = document.querySelector("#generate");
 
-//  The arrays of criteria the user can choose to generate a password
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-const lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const specials = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ']'];
+const criteria = {
+  numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+  lowerLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  upperLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  specials: ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ']'],
+  userChoices: [],
+  passwordLength: null,
 
-// The array of criteria the user chooses for the password
-let userChoices = [];
-
-// Sets global passwordLength variable
-let passwordLength;
+};
 
 //*** Begin Password Generation ***/
 
@@ -20,34 +18,35 @@ const intro = alert('Welcome to the Password Genterator!\nYou must choose from a
 const promptLowerCriteria = () => {
   let promptLower = prompt('Would you like to include lower-case letters (such as a,b,c)?\nPress OK if Yes or press Cancel if no');
   if (promptLower === '') {
-    userChoices.push(lowerLetters);
+    criteria.userChoices.push(criteria.lowerLetters);
   } 
 };
 
 const promptUpperCriteria = () => {
   let promptUpper = prompt('Would you like to include upper-case letters (such as A,B,C)?\nPress OK if Yes or press Cancel if no');
   if (promptUpper === '') {
-    userChoices.push(upperLetters);
+    criteria.userChoices.push(criteria.upperLetters);
   } 
 };
 
 const promptNumbersCriteria = () => {
   let promptNumbers = prompt('Would you like to include lower-case letters (such as 0,1,2)?\nPress OK if Yes or press Cancel if no');
   if (promptNumbers === '') {
-    userChoices.push(numbers);
+    criteria.userChoices.push(criteria.numbers);
   } 
 };
 
 const promptSpecialsCriteria = () => {
   let promptSpecials = prompt('Would you like to include lower-case letters (such as #,$,%)?\nPress OK if Yes or press Cancel if no');
   if (promptSpecials === '') {
-    userChoices.push(specials);
+    criteria.userChoices.push(criteria.specials);
   } 
 };
 
 const promptPasswordLength = () => {
   let promptLength = prompt('How long should your password be?\nYour password must be between 8 and 128 characters.');
-  passwordLength = parseInt(promptLength, 10);
+  criteria.passwordLength = parseInt(promptLength, 10);
+
   if ((promptLength >= 8) && (promptLength <= 128)) {
     
     alert('That length is valid!');
@@ -70,7 +69,7 @@ const finalMessage = alert('Great! We have all your criteria. Press the Generate
 
 // The function that will randomly select a member of the userChoices array
 const randomUserChoices = () => {
-  let mergedChoices = userChoices.flat(1);
+  let mergedChoices = criteria.userChoices.flat(1);
   return mergedChoices[Math.floor(Math.random() * mergedChoices.length)]
 };
 
@@ -79,7 +78,7 @@ const randomUserChoices = () => {
 const generatePassword = () => {
   let newPassword = [];
 
-  for (i = 0; i < passwordLength; i++) {
+  for (i = 0; i < criteria.passwordLength; i++) {
     newPassword.push(randomUserChoices());
   }
 
