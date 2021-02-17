c20264b44f9c24c2e347b7c417c80b52ea01d416
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-02-17 15:10:45 -0600

    Add comments. Fix words in numbers and specials prompt.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,7 @@
 // The button that initiates the password generator
 const generateBtn = document.querySelector("#generate");
 
+//Contains the arrays and variables needed to run the password generator
 const criteria = {
   numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
   lowerLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
@@ -17,8 +18,7 @@ const criteria = {
 
 //*** Begin Password Generation ***/
 
-const intro = alert('Welcome to the Password Genterator!\nYou must choose from at least one of these criteria:\nlowercase letter(a,b,d)\nUpper Case Letters(A,B,C)\nNumbers(0-9)\nSpecial characters(like #$%)\nYou must choose at least one criteria\n\nPress ok to continue');
-
+//Pushes lowerLetters array to userChoices array on ok.
 const promptLowerCriteria = () => {
   let promptLower = prompt('Would you like to include lower-case letters (such as a,b,c)?\nPress OK if Yes or press Cancel if no');
   criteria.lowerChoice = promptLower;
@@ -27,6 +27,7 @@ const promptLowerCriteria = () => {
   } 
 };
 
+//Pushes upperLetters array to userChoices array on ok.
 const promptUpperCriteria = () => {
   let promptUpper = prompt('Would you like to include upper-case letters (such as A,B,C)?\nPress OK if Yes or press Cancel if no');
   criteria.upperChoice = promptUpper;
@@ -35,29 +36,34 @@ const promptUpperCriteria = () => {
   } 
 };
 
+//Pushes numbers array to userChoices array on ok.
 const promptNumbersCriteria = () => {
-  let promptNumbers = prompt('Would you like to include lower-case letters (such as 0,1,2)?\nPress OK if Yes or press Cancel if no');
+  let promptNumbers = prompt('Would you like to include numbers (such as 0,1,2)?\nPress OK if Yes or press Cancel if no');
   numbersChoice = promptNumbers;
   if (promptNumbers === '') {
     criteria.userChoices.push(criteria.numbers);
   } 
 };
 
+//Pushes specials array to userChoices array on ok.
 const promptSpecialsCriteria = () => {
-  let promptSpecials = prompt('Would you like to include lower-case letters (such as #,$,%)?\nPress OK if Yes or press Cancel if no');
+  let promptSpecials = prompt('Would you like to include special characters (such as #,$,%)?\nPress OK if Yes or press Cancel if no');
   specialsChoice = promptSpecials;
   if (promptSpecials === '') {
     criteria.userChoices.push(criteria.specials);
   } 
 };
 
+//Executes the inital alert and criteria prompts
 const promptCriteria = () => {
+  const intro = alert('Welcome to the Password Genterator!\nYou must choose from at least one of these criteria:\nlowercase letter(a,b,d)\nUpper Case Letters(A,B,C)\nNumbers(0-9)\nSpecial characters(like #$%)\nYou must choose at least one criteria\n\nPress ok to continue');
   promptLowerCriteria();
   promptUpperCriteria();
   promptNumbersCriteria();
   promptSpecialsCriteria();
 };
 
+//Evaluates whether the user has clicked cancel on all criteria, forcing a re-execution of the promptCriteria() function.
 const validateCriteria = () => {
   if ((criteria.lowerChoice === null) && (criteria.upperChoice === null) && (criteria.numbersChoice === null) && (criteria.specialsChoice === null)) {
     alert('You must pick at least one criteria');
@@ -67,6 +73,7 @@ const validateCriteria = () => {
   }
 };
 
+//Executes the prompt for asking for password length, evaluates if it is valid, and returns the user back to the function if the length is not valid.
 const promptPasswordLength = () => {
   let promptLength = prompt('How long should your password be?\nYour password must be between 8 and 128 characters.');
   criteria.passwordLength = parseInt(promptLength, 10);
@@ -74,28 +81,28 @@ const promptPasswordLength = () => {
   if ((promptLength >= 8) && (promptLength <= 128)) {
     
     alert('That length is valid!');
+    const finalMessage = alert('Great! We have all your criteria. Press the Generate Password button below.');
   } else {
     alert('Your password must be between 8 and 128 characters.')
     promptPasswordLength();
   }
 };
 
+//Executes all the primary functions of the prompt script
 promptCriteria();
 validateCriteria();
 promptPasswordLength();
 
 
-const finalMessage = alert('Great! We have all your criteria. Press the Generate Password button below.');
 
-
-// The function that will randomly select a member of the userChoices array
+// Randomly selects a member of the userChoices array
 const randomUserChoices = () => {
   let mergedChoices = criteria.userChoices.flat(1);
   return mergedChoices[Math.floor(Math.random() * mergedChoices.length)]
 };
 
 
-// The function that generates the password
+// Generates the password based on passwordLength
 const generatePassword = () => {
   let newPassword = [];
 
@@ -107,7 +114,7 @@ const generatePassword = () => {
 };
 
 
-// This function taked the generated password and writes it to the #password id.
+// Takes the generated password and writes it to the #password id.
 const writePassword = () => {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
