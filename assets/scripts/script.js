a892458b3e705030a52d9093aa94bc7495b9300e
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-02-18 15:03:42 -0600

    Write functions to call each other in sequence.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,11 @@
-// The button that initiates the password generator
-const generateBtn = document.querySelector("#generate");
-
+//Javascript written with ES6 features such as const, let, and arrow functions to improve readability
 
 //Contains the arrays and variables needed to run the password generator
 const criteria = {
   numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
   lowerLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
   upperLetters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  specials: ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', ']'],
+  specials: ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
   userChoices: [],
   passwordLength: null,
   lowerChoice: null,
@@ -17,61 +15,84 @@ const criteria = {
 
 };
 
+// The button that initiates the password generator
+const generateBtn = document.querySelector("#generate");
+
+// Evaluates if the userChoices array is empty. If yes, will begin the password generation process. If false, will write the password to the textarea.
+const useButton = () => {
+  if (!criteria.userChoices[0]) {
+    beginPromptCriteria();
+  } else {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  
+};
+
+// Button listens for a click to begin either the password criteria process or password display
+generateBtn.addEventListener("click", useButton);
+
+
 //*** Begin Password Generation ***/
 
-//Pushes lowerLetters array to userChoices array on ok.
+//Executes the inital alert and lowerCriteria
+const beginPromptCriteria = () => {
+  alert('Welcome to the Password Genterator!\nYou must choose from at least one of these criteria:\nlowercase letter(a,b,d)\nUpper Case Letters(A,B,C)\nNumbers(0-9)\nSpecial characters(like #$%)\nYou must choose at least one criteria\n\nPress ok to continue');
+  promptLowerCriteria();
+
+};
+
+//Pushes lowerLetters array to userChoices array on ok then executes upperCritiera
 const promptLowerCriteria = () => {
   let promptLower = confirm('Would you like to include lower-case letters (such as a,b,c)?\nPress OK if Yes or press Cancel if no');
   criteria.lowerChoice = promptLower;
   if (promptLower) {
     criteria.userChoices.push(criteria.lowerLetters);
   } 
+  promptUpperCriteria();
 };
 
-//Pushes upperLetters array to userChoices array on ok.
+//Pushes upperLetters array to userChoices array on ok then executes Numbers criteria
 const promptUpperCriteria = () => {
   let promptUpper = confirm('Would you like to include upper-case letters (such as A,B,C)?\nPress OK if Yes or press Cancel if no');
   criteria.upperChoice = promptUpper;
   if (promptUpper) {
     criteria.userChoices.push(criteria.upperLetters);
-  } 
+  }
+  promptNumbersCriteria();
 };
 
-//Pushes numbers array to userChoices array on ok.
+//Pushes numbers array to userChoices array on ok then executes specials criteria
 const promptNumbersCriteria = () => {
   let promptNumbers = confirm('Would you like to include numbers (such as 0,1,2)?\nPress OK if Yes or press Cancel if no');
   criteria.numbersChoice = promptNumbers;
   if (promptNumbers) {
     criteria.userChoices.push(criteria.numbers);
   } 
+  promptSpecialsCriteria();
 };
 
-//Pushes specials array to userChoices array on ok.
+//Pushes specials array to userChoices array on ok then executes criteria validation
 const promptSpecialsCriteria = () => {
   let promptSpecials = confirm('Would you like to include special characters (such as #,$,%)?\nPress OK if Yes or press Cancel if no');
   criteria.specialsChoice = promptSpecials;
   if (promptSpecials) {
     criteria.userChoices.push(criteria.specials);
   } 
+  validateCriteria();
 };
 
-//Executes the inital alert and criteria prompts
-const promptCriteria = () => {
-  alert('Welcome to the Password Genterator!\nYou must choose from at least one of these criteria:\nlowercase letter(a,b,d)\nUpper Case Letters(A,B,C)\nNumbers(0-9)\nSpecial characters(like #$%)\nYou must choose at least one criteria\n\nPress ok to continue');
-  promptLowerCriteria();
-  promptUpperCriteria();
-  promptNumbersCriteria();
-  promptSpecialsCriteria();
-};
-
-//Evaluates whether the user has clicked cancel on all criteria, forcing a re-execution of the promptCriteria() function.
+//Evaluates whether the user has clicked cancel on all criteria, forcing a re-execution of the beginPromptCriteria() function. Then executes password length prompt
 const validateCriteria = () => {
   if ((criteria.lowerChoice === false) && (criteria.upperChoice === false) && (criteria.numbersChoice === false) && (criteria.specialsChoice === false)) {
     alert('You must pick at least one criteria');
-    promptCriteria();
+    beginPromptCriteria();
   } else {
     alert('Great! You have picked at least one criteria.');
   }
+  promptPasswordLength();
 };
 
 //Executes the prompt for asking for password length, evaluates if it is valid, and returns the user back to the function if the length is not valid.
@@ -88,13 +109,6 @@ const promptPasswordLength = () => {
     promptPasswordLength();
   }
 };
-
-
-//Executes all the primary functions of the prompt script
-promptCriteria();
-validateCriteria();
-promptPasswordLength();
-
 
 
 // Randomly selects a member of the userChoices array
@@ -115,17 +129,6 @@ const generatePassword = () => {
   return newPassword.join('');
 };
 
-
-// Takes the generated password and writes it to the #password id.
-const writePassword = () => {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-} 
-
-// Adds event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 
